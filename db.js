99f3fb2650b900da45f6077502f7ba8206dbf2db
Mashood <mashoodkk2006@gmail.com>
2026-03-07 03:03:16 +0530

    fix mysql connection pool

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to Railway MySQL database");
-  }
-});
+console.log("MySQL pool connected");
 
-module.exports = db;
+module.exports = db.promise();
