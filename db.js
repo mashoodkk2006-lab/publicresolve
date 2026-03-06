3ce86bda401e0d891dfdd85238379dab4a59d836
Mashood <mashoodkk2006@gmail.com>
2026-03-07 01:56:14 +0530

    fix railway database connection

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Aaq@7747", // add your MySQL password if you have
-  database: "complaint_system"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("Connected to MySQL database");
+    console.log("Connected to Railway MySQL database");
   }
 });
 
